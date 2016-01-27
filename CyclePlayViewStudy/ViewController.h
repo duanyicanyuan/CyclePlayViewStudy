7b7f8026c62e0e368063c7a8f5694c4c0915ed47
caiqiujun <903400858@qq.com>
2016-01-27 09:24:43 +0800

    Initial Commit

---
+++ CyclePlayViewStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyclePlayViewStudy
+//
+//  Created by caiqiujun on 16/1/27.
+//  Copyright © 2016年 caiqiujun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CyclePlayViewStudy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CyclePlayViewStudy
+//
+//  Created by caiqiujun on 16/1/27.
+//  Copyright © 2016年 caiqiujun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
